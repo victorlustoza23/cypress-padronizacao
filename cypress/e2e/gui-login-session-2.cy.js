7305087796b70928ce872680af6afce228526b03
victorlustoza23 <victorlustoza23@gmail.com>
2026-01-09 11:03:24 -0300

    Adding cy.visit on gui-login-session-2 spec

--- cypress/e2e/gui-login-session-2.cy.js
+++ cypress/e2e/gui-login-session-2.cy.js
@@ -12,7 +12,7 @@ describe('Login2 - Validate cacheAcrossSpecs - login-session-2.cy.js', () => {
     { tags: '@login' },
     () => {
       cy.gui_login(user, password, options)
-      cy.visit(Cypress.env('MADEIRAMADEIRA_PRODUCTION_URL'))
+      cy.visit(Cypress.env('MADEIRAMADEIRA_PRODUCTION_URL'), { timeout: 7000 })
 
       cy.gui_loginValidation()
     },
@@ -23,6 +23,7 @@ describe('Login2 - Validate cacheAcrossSpecs - login-session-2.cy.js', () => {
     { tags: '@example-fail' },
     () => {
       cy.gui_login(user, password, options)
+      cy.visit(Cypress.env('MADEIRAMADEIRA_PRODUCTION_URL'), { timeout: 7000 })
 
       cy.contains('Minha Conta').should('be.visible').click()
       cy.contains('span', 'Sairr').should('be.visible')
